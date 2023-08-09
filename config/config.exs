fc1957fae3e6251842753d957091c528735cc93c
Alicia Gonzalez <alicia.gonzalez.90@gmail.com>
2023-08-09 21:54:37 +0200

    Setup mailer config

--- config/config.exs
+++ config/config.exs
@@ -5,9 +5,19 @@ config :crawly,
     {Crawly.Middlewares.UserAgent,
      user_agents: [
        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-     ]},
+     ]}
   ],
   pipelines: [
     Crawly.Pipelines.JSONEncoder,
     {Crawly.Pipelines.WriteToFile, extension: "jl", folder: "tmp"}
   ]
+
+config :dealz, Dealz.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: System.get_env("SES_SERVER"),
+  port: System.get_env("SES_PORT"),
+  username: System.get_env("SMTP_USERNAME"),
+  password: System.get_env("SMTP_PASSWORD"),
+  auth: :if_available,
+  tls_verify: :verify_none
+
